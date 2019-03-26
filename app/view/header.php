30da9d11202b54dd5bbd401f7829a594108ed5eb
Schaer Noah <schaernoa@ict-campus.net>
2019-03-26 10:16:45 +0100

    Burger-Menu Images fixed (after login)

--- app/view/header.php
+++ app/view/header.php
@@ -21,8 +21,9 @@ if($_SESSION['benutzer_app']){
             <a href="home">
                 <img class="img_header_two" src="./image/postlogo.png" alt="postlogo">
             </a>
-            <div class="hamburg">
-                <img id="img_header_burger" src="./image/burger_closed.png" alt="Menu">
+            <div id="burger" class="hamburg">
+                <img class="bottom transparent" src="./image/burger_opened.png" alt="Menu">
+                <img class="top" src="./image/burger_closed.png" alt="Menu">
             </div>
         </nav>
     </header>';
@@ -56,33 +57,6 @@ else{
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
-    /*
-    var hamburger = document.querySelector(".hamburg");
-    
-    hamburger.onclick = function () {
-        if (this.classList.contains("checked")) {
-            this.classList.remove("checked");
-            $("#burger img.top").toggleClass("transparent");
-            $("#burger img.bottom").toggleClass("transparent");
-            closeNav();
-        }
-        else {
-            this.classList.add("checked");
-            $("#burger img.top").toggleClass("transparent");
-            $("#burger img.bottom").toggleClass("transparent");
-            openNav();
-        }
-    }*/
-
-    function openNav() {
-        document.getElementById("div_navigation").style.width = "100%";
-        document.getElementsByTagName("body")[0].style = 'overflow-y: hidden;'
-    }
-    function closeNav() {
-        document.getElementById("div_navigation").style.width = "0%";
-        document.getElementsByTagName("body")[0].style = 'overflow-y: visible;'
-    }
-
     $(document).ready(function() {
         $("#burger").click(function() {
             $("#burger img").toggleClass("transparent");
@@ -102,4 +76,13 @@ else{
             }
         });
     });
+
+    function openNav() {
+        document.getElementById("div_navigation").style.width = "100%";
+        document.getElementsByTagName("body")[0].style = 'overflow-y: hidden;'
+    }
+    function closeNav() {
+        document.getElementById("div_navigation").style.width = "0%";
+        document.getElementsByTagName("body")[0].style = 'overflow-y: visible;'
+    }
 </script>
